eccdda1d0827ac81b172ef9b33537a434780f81e
nosoosso <nosoosso.null@gmail.com>
2022-01-21 23:27:02 +0900

    fix secondsToTimeString function
    時間がマイナスになったときの表示がおかしかったのを修正

--- utils.js
+++ utils.js
@@ -1,15 +1,22 @@
 import {useEffect, useRef} from "react"
 
+/**
+ * 秒数を HH:mm:ss の形式の文字列に変換する
+ * マイナスの秒数を渡した場合、結果もマイナスになる
+ */
 export function secondsToTimeString(timeSeconds) {
-  const hours = (timeSeconds >= 3600) ? Math.floor(timeSeconds / 3600) : 0
-  const minutes = (timeSeconds >= 60) ? (Math.floor(timeSeconds / 60)) % 60 : 0
-  const seconds = timeSeconds % 60
+  const isPositive = timeSeconds >= 0
+  const absSeconds = Math.abs(timeSeconds)
+
+  const hours = (absSeconds >= 3600) ? Math.floor(absSeconds / 3600) : 0
+  const minutes = (absSeconds >= 60) ? (Math.floor(absSeconds / 60)) % 60 : 0
+  const seconds = absSeconds % 60
 
   const hoursStr = hours.toString().padStart(2, "0")
   const minutesStr = minutes.toString().padStart(2, "0")
   const secondsStr = seconds.toString().padStart(2, "0")
 
-  return `${hoursStr}:${minutesStr}:${secondsStr}`
+  return `${isPositive ? "" : "-"}${hoursStr}:${minutesStr}:${secondsStr}`
 }
 
 export function usePrevious(value) {
