403f77531dffb54d834dd0cf3be0666b8d9a5749
nosoosso <nosoosso.null@gmail.com>
2022-06-23 21:36:18 +0900

    add comment

--- utils.js
+++ utils.js
@@ -19,6 +19,9 @@ export function secondsToTimeString(timeSeconds) {
   return `${isPositive ? "" : "-"}${hoursStr}:${minutesStr}:${secondsStr}`
 }
 
+/**
+ * https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
+ */
 export function usePrevious(value) {
   const ref = useRef();
   useEffect(() => {
